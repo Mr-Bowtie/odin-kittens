c1d6cb954d9402db81cdf27a3c0fdfc5d354fab0
Mr-Bowtie <mr.bowtie989@gmail.com>
2022-05-24 06:46:46 -0400

    add validations on cuteness and softness

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,4 @@
 class Kitten < ApplicationRecord
+  validates :cuteness, inclusion: { in: (1..10), message: "Must be between 1 and 10" }
+  validates :softness, inclusion: { in: (1..10), message: "Must be between 1 and 10" }
 end
